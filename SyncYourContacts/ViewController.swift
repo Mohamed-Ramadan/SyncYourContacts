58154c908f9eb83a1236704664ae3f4e86d6bb73
Mohamed.Ramadan_ios <ramadan@isafte-land.com>
2021-09-25 13:14:28 +0200

    Initial Commit

---
+++ SyncYourContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SyncYourContacts
+//
+//  Created by Mohamed Ramadan on 25/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
